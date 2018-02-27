0408f4d509962f7ef7c6b183ce055a7653b103ea
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-02-27 07:48:01 +0100

    Added error control

--- wait.c
+++ wait.c
@@ -1,11 +1,23 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
 
+	size_t len = strlen(argv[1]);
+	if (len + 21 > 256) {
+		fprintf(stderr, "Error: name is too long\n");
+		return 1;
+	}
+
 	char buf[256];
-	sprintf(buf, "pidof -x %s > /dev/null", argv[1]);
+	int r = sprintf(buf, "pidof -x %s > /dev/null", argv[1]);
+
+	if (r < 0) {
+		fprintf(stderr, "Error when creating buffer\n");
+		return 1;
+	}
 
 	while (system(buf) == 0) {
 		sleep(1.0);
